8b52a76549ba562bc3083fda4d1d6f2fd7585eb8
Kartikkhode <kartikkhode21@gmail.com>
2022-12-17 11:10:23 +0530

    Add files via upload

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,9 +5,9 @@ const stripe = Stripe('pk_test_51MCgFoSAAUTIQMxNFhZzOxkSKy2vD5icPN1iINRokZZjzKsl
 export const bookTour = async tourId => {
     //get the session from api
     try{const session = await axios(
-        `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`  
+        `/api/v1/bookings/checkout-session/${tourId}`  
     );
-    console.log(session);
+    // console.log(session);
     //create checkout form + chanre credit card
         await stripe.redirectToCheckout({
             sessionId: session.data.session.id
